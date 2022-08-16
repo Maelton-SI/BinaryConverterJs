caed9f9764db544af2784683e4ebaeff3cd00266
Maelton-SI <mls54@aluno.ifal.edu.br>
2022-08-16 13:24:43 -0300

    binaryToOctal method added.

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/**
+ Binary to Decimal => ok
+ Decimal to Binary => ok
+ 
+ Binary to Octal => ok
+ Octal to Binary
+
+ Binary to Hexadecimal
+ Hexadecimal to Binary
+
+ Octal to Decimal
+ Decimal to Octal
+
+ Hexadecimal to Decimal
+ Hexadecimal to Binary
+ */
+
 //Converts decimal numbers to binary numbers
 function decimalToBinary(number) { 
     return number==0 ? "" : decimalToBinary(~~(number/2)) + String(number%2) 
@@ -7,3 +24,19 @@ function decimalToBinary(number) {
 function binaryToDecimal(number) { 
     return number.length==1 ? Number(number) : Number(number[0]) * 2 ** (number.length-1) + binaryToDecimal(number.substring(1))
 }
+
+//Converts binary numbers to octal
+function binaryToOctal(number) {
+    if (number.length <= 3) {
+        return String( binaryToDecimal(number) )
+    } else {
+        return binaryToOctal(number.slice(0, -3)) + String( binaryToDecimal(number.slice(-3)) )
+    }
+}
+
+function tests() {
+    let result = binaryToOctal('11110')
+    console.log(result)
+}
+
+tests()
